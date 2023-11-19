9ea31f63cc598dcad48e9203e2620148c3824ef9
Tanya Lisna <tplovic@gmail.com>
2023-11-19 14:21:54 +0200

    deploy: changes

--- src/test/test-utils.ts
+++ src/test/test-utils.ts
@@ -1,11 +1,10 @@
 import { ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
-import RootProvider from '../context/RootProvider';
 
 const itemsRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'wrapper'>
-) => render(ui, { wrapper: RootProvider, ...options });
+) => render(ui, { ...options });
 
 export * from '@testing-library/react';
 export { itemsRender as render };
