2788a0bd95b541fd00e8cfbf4bc937a16fd15d91
Tanya Lisna <tplovic@gmail.com>
2023-11-24 12:08:35 +0200

    Updates

--- mockServiceWorker.js
+++ mockServiceWorker.js
@@ -2,7 +2,7 @@
 /* tslint:disable */
 
 /**
- * Mock Service Worker (2.0.5).
+ * Mock Service Worker (2.0.8).
  * @see https://github.com/mswjs/msw
  * - Please do NOT modify this file.
  * - Please do NOT serve this file on production.
@@ -75,7 +75,6 @@ self.addEventListener('message', async function (event) {
         return client.id !== clientId;
       });
 
-      // Unregister itself when there are no more clients
       if (remainingClients.length === 0) {
         self.registration.unregister();
       }
@@ -88,25 +87,18 @@ self.addEventListener('message', async function (event) {
 self.addEventListener('fetch', function (event) {
   const { request } = event;
 
-  // Bypass navigation requests.
   if (request.mode === 'navigate') {
     return;
   }
 
-  // Opening the DevTools triggers the "only-if-cached" request
-  // that cannot be handled by the worker. Bypass such requests.
   if (request.cache === 'only-if-cached' && request.mode !== 'same-origin') {
     return;
   }
 
-  // Bypass all requests when there are no active clients.
-  // Prevents the self-unregistered worked from handling requests
-  // after it's been deleted (still remains active until the next reload).
   if (activeClientIds.size === 0) {
     return;
   }
 
-  // Generate unique request ID.
   const requestId = crypto.randomUUID();
   event.respondWith(handleRequest(event, requestId));
 });
@@ -115,16 +107,9 @@ async function handleRequest(event, requestId) {
   const client = await resolveMainClient(event);
   const response = await getResponse(event, client, requestId);
 
-  // Send back the response clone for the "response:*" life-cycle events.
-  // Ensure MSW is active and ready to handle the message, otherwise
-  // this message will pend indefinitely.
   if (client && activeClientIds.has(client.id)) {
     (async function () {
       const responseClone = response.clone();
-      // When performing original requests, response body will
-      // always be a ReadableStream, even for 204 responses.
-      // But when creating a new Response instance on the client,
-      // the body for a 204 response must be null.
       const responseBody = response.status === 204 ? null : responseClone.body;
 
       sendToClient(
@@ -149,10 +134,6 @@ async function handleRequest(event, requestId) {
   return response;
 }
 
-// Resolve the main client for the given event.
-// Client that issues a request doesn't necessarily equal the client
-// that registered the worker. It's with the latter the worker should
-// communicate with during the response resolving phase.
 async function resolveMainClient(event) {
   const client = await self.clients.get(event.clientId);
 
@@ -166,12 +147,9 @@ async function resolveMainClient(event) {
 
   return allClients
     .filter((client) => {
-      // Get only those clients that are currently visible.
       return client.visibilityState === 'visible';
     })
     .find((client) => {
-      // Find the client ID that's recorded in the
-      // set of clients that have registered the worker.
       return activeClientIds.has(client.id);
     });
 }
@@ -179,42 +157,29 @@ async function resolveMainClient(event) {
 async function getResponse(event, client, requestId) {
   const { request } = event;
 
-  // Clone the request because it might've been already used
-  // (i.e. its body has been read and sent to the client).
   const requestClone = request.clone();
 
   function passthrough() {
     const headers = Object.fromEntries(requestClone.headers.entries());
 
-    // Remove internal MSW request header so the passthrough request
-    // complies with any potential CORS preflight checks on the server.
-    // Some servers forbid unknown request headers.
     delete headers['x-msw-intention'];
 
     return fetch(requestClone, { headers });
   }
 
-  // Bypass mocking when the client is not active.
   if (!client) {
     return passthrough();
   }
 
-  // Bypass initial page load requests (i.e. static assets).
-  // The absence of the immediate/parent client in the map of the active clients
-  // means that MSW hasn't dispatched the "MOCK_ACTIVATE" event yet
-  // and is not ready to handle requests.
   if (!activeClientIds.has(client.id)) {
     return passthrough();
   }
 
-  // Bypass requests with the explicit bypass header.
-  // Such requests can be issued by "ctx.fetch()".
   const mswIntention = request.headers.get('x-msw-intention');
   if (['bypass', 'passthrough'].includes(mswIntention)) {
     return passthrough();
   }
 
-  // Notify the client that a request has been intercepted.
   const requestBuffer = await request.arrayBuffer();
   const clientMessage = await sendToClient(
     client,
@@ -273,10 +238,6 @@ function sendToClient(client, message, transferrables = []) {
 }
 
 async function respondWithMock(response) {
-  // Setting response status code to 0 is a no-op.
-  // However, when responding with a "Response.error()", the produced Response
-  // instance will have status code set to 0. Since it's not possible to create
-  // a Response instance with status code 0, handle that use-case separately.
   if (response.status === 0) {
     return Response.error();
   }
