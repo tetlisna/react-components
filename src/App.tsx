a6e57d326528e1af0e6d3f5ca5f225c31c728f8f
Tanya Lisna <tplovic@gmail.com>
2023-11-30 14:13:25 +0200

    init: configure react-from project with vite, eslint, prettier and  husky

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>fffff</div>
+      <h1>Vite + React</h1>
+
+          <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
